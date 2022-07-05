3bd338aade3ba28a1c9e5586b6d2581062a2a343
abdulrahman metwally <80607902+metwalla@users.noreply.github.com>
2022-07-06 01:42:38 +0300

    Update RegestrationForm.js

--- components/RegestrationForm.js
+++ components/RegestrationForm.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import reg from './reg.css'
-export default function Form(){
-    return(
-        <section>
-            <div className='register'>
-                <div className='col-1'>
-                   <h2>Sign up</h2>
-                   <span>regestration form</span>
-                   <form id='form' classname='flex flex-col'>
-                    <input type='text' placeholder='Email'/>
-                    <input type='text' placeholder='First name'>
-                        <input type='text' placeholder='Last name'>
-                            <input type='text' placeholder='password'>
-                                <input type='text' placeholder='reenter password'>
-
-                                </input>
-                            </input>
-                        </input>
-                    </input>
-                   </form>
-                   <button className='btn'>Sign up</button>
-                </div>
-            </div>
-        </section>
-    )
+import React from "react";
+import "./reg.css";
+export default function Form() {
+  return (
+    <section>
+      <div className="register">
+        <div className="col-1">
+          <h2>Sign up</h2>
+          <span>registration form</span>
+          <form id="form" classname="flex flex-col">
+            <input type="text" placeholder="Email"></input>
+            <input type="text" placeholder="First name"></input>
+            <input type="text" placeholder="Last name"></input>
+            <input type="text" placeholder="password"></input>
+            <input type="text" placeholder="reenter password"></input>
+          </form>
+          <button className="btn">Sign up</button>
+        </div>
+      </div>
+    </section>
+  );
 }
